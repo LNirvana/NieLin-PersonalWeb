061c6c9a180967c2d2511581e4170e254efe944c
1422459627@qq.com <1422459627@qq.com>
2021-06-06 02:05:52 +0800

    update my web in 20210606

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Profile></Profile>
+    <info-list></info-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Header from "./components/Header.vue";
+import InfoList from "./components/InfoList.vue";
+import Profile from "./components/Profile.vue";
+
+@Component({
+  components: {
+    Header,
+    Profile,
+    InfoList,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss"></style>
